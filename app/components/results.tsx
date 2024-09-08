a79d0d7ad3471747da48cba72652a0a7b9f73eb7
GabrielRQueiroz <gabrielr.queiroz@gmail.com>
2024-09-07 22:05:21 -0300

    Opção de alterar entre modelos

--- app/components/results.tsx
+++ app/components/results.tsx
@@ -4,67 +4,111 @@ import { HfInference } from "@huggingface/inference";
 import clsx from "clsx";
 import Image from "next/image";
 import { useRef, useState } from "react";
+import { toast } from "react-toastify";
+
+type Developer = string
+type ModelUrl = string
 
 const HF_TOKEN = process.env.NEXT_PUBLIC_HF_API_KEY;
 
+const modelsList: Record<Developer, ModelUrl> = {
+  'Stability AI - Stable Diffusion XL': 'stabilityai/stable-diffusion-xl-base-1.0',
+  'Stability AI - Stable Diffusion Turbo': 'stabilityai/sdxl-turbo',
+  'CompVis - Stable Diffusion': 'CompVis/stable-diffusion-v1-4',
+  'Black Forest Labs - Flux': 'black-forest-labs/FLUX.1-dev',
+  'XLabs AI - Flux': 'XLabs-AI/flux-RealismLora',
+  'Prompt Hero - Open Journey': 'prompthero/openjourney-v4',
+}
+
 export const Results = () => {
-  const [results, setResults] = useState<Blob>()
+  const [results, setResults] = useState<string>('');
+  const [model, setModel] = useState<ModelUrl>(modelsList['Stability AI - Stable Diffusion XL'])
+  const [promptTitle, setPromptTitle] = useState<string>('')
   const [isLoading, setIsLoading] = useState<boolean>(false)
   const inputRef = useRef<HTMLInputElement>(null);
+  const imageRef = useRef<HTMLImageElement>(null);
 
   const inference = new HfInference(HF_TOKEN);
 
   const generateImage = async (e: React.MouseEvent<HTMLButtonElement>) => {
     e.preventDefault();
 
-    if (!inputRef.current?.value) return;
+    if (!inputRef.current?.value) return toast.warning('Digite algo para gerar a imagem.');
 
     try {
+      toast.info('Gerando imagem...');
       setIsLoading(true);
       const response = await inference.textToImage({
-        model: 'CompVis/stable-diffusion-v1-4',
+        model: model,
         inputs: inputRef.current.value,
-        parameters: {
-          negative_prompt: 'blurry',
-        }
-      })
+      }).catch((error) => {
+        throw new Error(error)
+      });
+      toast.success('Imagem gerada com sucesso!');
+      setPromptTitle(inputRef.current.value);
       setIsLoading(false);
 
-      setResults(response);
+      setResults(URL.createObjectURL(response));
+
+      imageRef.current?.scrollIntoView({ behavior: "smooth", block: "center" })
       return
-    } catch (error) {
-      alert(error);
+    } catch (error: any) {
+      setIsLoading(false);
+
+      toast.error(`Modelo indisponível no momento. Tente escolher outro.\nMensagem de erro: ${error}`);
     }
 
   }
 
   return (
-    <>
-      <form action='post' className="bg-stone-900 gap-2 rounded-xl border-2 border-gray-700 transition-all duration-100 focus-within:border-blue-800 mx-auto text-xl mt-96 w-4/5 relative flex items-center justify-center h-20">
+    <div className="mt-80 mx-auto flex flex-col gap-4 w-4/5">
+      <div className="flex flex-row items-center justify-left w-3/5">
+        <h2 className="text-lg font-bold">Modelo:</h2>
+        <select value={model} onChange={(e) => setModel(e.target.value)} className="p-1 flex-1 ml-4 rounded-lg bg-transparent border-2 border-gray-700 transition-all duration-100 focus-within:border-blue-800">
+          {
+            Object.entries(modelsList).map(([developer, modelUrl]) => (
+              <option className='bg-stone-900' key={developer} value={modelUrl}>
+                {developer}
+              </option>
+            ))
+          }
+        </select>
+      </div>
+
+      <form action='post' className="bg-stone-900 gap-2 rounded-xl border-2 border-gray-700 transition-all duration-100 focus-within:border-blue-800 mx-auto text-xl w-full relative flex items-center justify-center h-16">
         <label className="sr-only" htmlFor="search-input">Gerar imagem</label>
         <input ref={inputRef} className="bg-transparent pl-4 h-full w-full outline-none" type="text" id="search-input" />
         <button onClick={generateImage}
           disabled={isLoading}
           className={clsx(
-            "bg-stone-950 mr-4 shadow-sm right-0 py-3 px-10 border-2 border-blue-900 rounded transition-all duration-100",
-            isLoading && "opacity-50 cursor-not-allowed",
+            "bg-stone-950 mr-3 shadow-sm right-0 py-2 px-10 border-2 border-blue-900 rounded-md transition-all duration-100",
+            isLoading && "opacity-50 cursor-wait",
             !isLoading && "hover:bg-blue-900"
           )}>
           Gerar
         </button>
       </form>
 
-      <section className="flex items-center justify-center py-12">
+      <section className="flex flex-col gap-4 min-h-48 items-center justify-center pt-6 pb-16 overflow-x-hidden">
         {
           isLoading ?
-            <div className="animate-spin absolute w-16 h-16 border-4 border-t-white border-solid rounded-full" />
+            <div className="animate-spin absolute w-16 h-16 border-4 border-t-blue-800 border-r-blue-600 border-b-blue-400 border-l-blue-200 border-solid rounded-full" />
             :
             results &&
-            <figure className="relative w-96 h-96">
-              <Image fill className="object-cover rounded-lg" src={URL.createObjectURL(results)} alt="Generated Image" />
-            </figure>  // URL.createObjectURL is used to create a blob URL from a Blob object
+            <>
+              <figcaption className="text-3xl text-center font-bold">🎨 {promptTitle}</figcaption>
+              <figure className="relative w-96 h-96">
+                <Image ref={imageRef} fill className="object-cover rounded-lg" src={results} alt="Generated Image" />
+              </figure>
+              {/* make it look like a button */}
+              <a href={results} download={`${promptTitle}.png`}
+                className="hover:bg-blue-950 py-3 px-10 rounded-md transition-all duration-100 bg-blue-900"
+              >
+                Baixar Imagem 📥
+              </a>
+            </>
         }
       </section>
-    </>
+    </div>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,8 @@
 import Image from "next/image";
+import { Bounce, ToastContainer } from "react-toastify";
 import { Results } from "./components/results";
+
+import 'react-toastify/dist/ReactToastify.min.css';
 
 export default function Home() {
   return (
@@ -7,7 +10,7 @@ export default function Home() {
       <nav className="fixed flex mx-auto items-center justify-between top-0 left-0 w-full px-8 py-4">
         <div id="logo" className="flex items-center gap-2">
           <figure className="h-12 w-12 relative">
-            <Image className="object-contain" alt="Logo" src="/logo.png" fill/>
+            <Image className="object-contain" alt="Logo" src="/logo.png" fill />
           </figure>
           <p className="font-bold text-xl">
             Figurificadoríssimo
@@ -15,7 +18,7 @@ export default function Home() {
         </div>
       </nav>
 
-      <div className="pointer-events-none h-96 -z-10 max-h-fit absolute w-screen top-0 left-0 overflow-y-hidden flex flex-col items-center justify-center">
+      <div className="pointer-events-none h-96 -z-10 max-h-fit absolute w-full top-0 left-0 overflow-y-hidden flex flex-col items-center justify-center">
         <h2 className="font-bold text-4xl">
           Gere imagens com IA
           <span className="my-2 block text-center text-blue-300 text-5xl">
@@ -23,12 +26,25 @@ export default function Home() {
           </span>
         </h2>
         <video tabIndex={-1} className="opacity-50 top-0 absolute mx-auto -z-20 w-screen" controls preload="true" autoPlay loop muted>
-          <source src="/waves.mp4" type="video/mp4" /> 
+          <source src="/waves.mp4" type="video/mp4" />
         </video>
         <div className="z-10 absolute w-full h-1/2 bottom-0 bg-gradient-to-t from-black to-transparent"></div>
       </div>
 
       <Results />
+      <ToastContainer
+        position="bottom-right"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss={false}
+        draggable
+        pauseOnHover
+        theme="colored"
+        transition={Bounce}
+      />
     </main>
   );
 }
